36767049f472eb0e29a283b9c33fd494db0e3357
Christine <christine.altmann@gmx.com>
2019-10-27 21:04:19 +0000

    add test to ensure correct output and output format when system date and date to format don't match

--- __tests__/dateFormatterTest.js
+++ __tests__/dateFormatterTest.js
@@ -1,8 +1,16 @@
-import {format} from '../src/dateFormatter';
+import { format } from "../src/dateFormatter";
+
+const December = 11; //js Date object month is indexed from 0
+const January = 0;
 
 it('when the system date and the date to format are the same day formats as "TODAY"', () => {
-  const December = 11; //js Date object month is indexed from 0
   const systemDateTime = new Date(2018, December, 15, 10, 5).getTime();
   const dateTimeToFormat = new Date(2018, December, 15, 12, 50).getTime(); //Same day different time
-  expect(format(dateTimeToFormat, systemDateTime)).toBe('TODAY');
+  expect(format(dateTimeToFormat, systemDateTime)).toBe("TODAY");
+});
+
+it("when the system date and the date to format do not match, return date to format in following format 'DD/MM/YYYY'", () => {
+  const systemDateTime = new Date(2018, December, 15, 10, 5).getTime();
+  const dateTimeToFormat = new Date(2019, January, 15, 12, 50).getTime();
+  expect(format(dateTimeToFormat, systemDateTime)).toBe("15/01/2019");
 });
