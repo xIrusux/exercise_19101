60d63b5c135850fee0b82abeb11f8abddacc7718
Christine <christine.altmann@gmx.com>
2019-10-27 21:57:38 +0000

    create functions to pass both tests with updated declarative naming

--- __tests__/dateFormatterTest.js
+++ __tests__/dateFormatterTest.js
@@ -1,4 +1,4 @@
-import { format } from "../src/dateFormatter";
+import { compareDates } from "../src/dateFormatter";
 
 const December = 11; //js Date object month is indexed from 0
 const January = 0;
@@ -6,11 +6,11 @@ const January = 0;
 it('when the system date and the date to format are the same day formats as "TODAY"', () => {
   const systemDateTime = new Date(2018, December, 15, 10, 5).getTime();
   const dateTimeToFormat = new Date(2018, December, 15, 12, 50).getTime(); //Same day different time
-  expect(format(dateTimeToFormat, systemDateTime)).toBe("TODAY");
+  expect(compareDates(dateTimeToFormat, systemDateTime)).toBe("TODAY");
 });
 
 it("when the system date and the date to format do not match, return date to format in following format 'DD/MM/YYYY'", () => {
   const systemDateTime = new Date(2018, December, 15, 10, 5).getTime();
   const dateTimeToFormat = new Date(2019, January, 15, 12, 50).getTime();
-  expect(format(dateTimeToFormat, systemDateTime)).toBe("15/01/2019");
+  expect(compareDates(dateTimeToFormat, systemDateTime)).toBe("15/01/2019");
 });
--- src/dateFormatter.js
+++ src/dateFormatter.js
@@ -1,3 +1,27 @@
-export const format = (dateToFormatTimeMillis, systemDateTimeMillis) => {
-  return '';
+// format date in milliseconds to a date string 'DD/MM/YYYY'
+
+const formatToDateString = dateTimeMillis => {
+  const dateInstance = new Date(dateTimeMillis);
+  const year = dateInstance.getFullYear();
+  const month = (dateInstance.getMonth() + 1).toString();
+  const day = dateInstance.getDate().toString();
+
+  const twoDigitMonth = month.length > 1 ? month : "0" + month;
+  const twoDigitDay = day.length > 1 ? day : "0" + day;
+
+  const dateString = twoDigitDay + "/" + twoDigitMonth + "/" + year;
+
+  return dateString;
 };
+
+// compare the two dateStrings and return "TODAY" if they match
+// if the first argument does not match the second, return the first argumet in format 'DD/MM/YYYY'
+
+const compareDates = (dateToFormatTimeMillis, systemDateTimeMillis) => {
+  let systemDateString = formatToDateString(systemDateTimeMillis);
+  let dateToFormatString = formatToDateString(dateToFormatTimeMillis);
+
+  return dateToFormatString === systemDateString ? "TODAY" : dateToFormatString;
+};
+
+export { compareDates };
